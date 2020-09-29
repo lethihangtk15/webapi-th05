73cddf073cd2cb36dd76764220e3f4b873fc012d
lethihangtk15 <70497474+lethihangtk15@users.noreply.github.com>
2020-09-29 23:33:35 +0700

    update

---
+++ TH05/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TH05.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TH05/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TH05.Models;
+
+namespace TH05.Controllers
+{
+    public class HomeController : Controller
+    {
+        public StudentsController st = new StudentsController();
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult student()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View(st.GetStudents());
+        }
+        public ActionResult ADD(string id, string first, string last, string stan)
+        {
+            StudentsController sv = new StudentsController();
+            Student item = new Student();
+            item.StudentID = id;
+            item.FirstName = first;
+            item.LastName = last;
+            item.StandardId = stan;
+            sv.PostStudent(item);
+            
+            return Redirect("~/Home/Student");
+        }
+    }
+}
